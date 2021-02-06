299f0bfee2655cd15ba471c8bc43fa8f572f89f2
spagnuolo <spagnuol@stud.fra-uas.de>
2021-02-06 20:57:47 +0100

    removed setting user name manually

--- chaincode/application/sell.js
+++ chaincode/application/sell.js
@@ -1,10 +1,4 @@
 /*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
-*/
-
-/*
  * This application has 6 basic steps:
  * 1. Select an identity from a wallet
  * 2. Connect to network gateway
@@ -22,84 +16,54 @@ const yaml = require('js-yaml');
 const { Wallets, Gateway } = require('fabric-network');
 const Energy = require('../contract/lib/energy.js');
 
-// Main program function
 async function main() {
-    // Specify userName for network access
-    // const userName = 'viet.seller@kunde.com';
-    const userName = process.argv[2];
-    if (!userName) {
-        console.log('Please provide an user name.');
-        return;
-    }
-    // A wallet stores a collection of identities for use
-    const wallet = await Wallets.newFileSystemWallet(`../identity/user/${userName}/wallet`);
+    let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/connection.yaml', 'utf8'));
+    const organization = connectionProfile.client.organization;
+    const userName = 'user' + organization;
 
-    // A gateway defines the peers used to access Fabric networks
+    const wallet = await Wallets.newFileSystemWallet(`../identity/user/${userName}/wallet`);
     const gateway = new Gateway();
 
-    // Main try/catch block
+    let connectionOptions = {
+        identity: userName,
+        wallet: wallet,
+        discovery: { enabled: true, asLocalhost: true }
+
+    };
+
     try {
-        // Load connection profile; will be used to locate a gateway
-        let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/connection.yaml', 'utf8'));
-
-        // Set connection options; identity and wallet
-        let connectionOptions = {
-            identity: userName,
-            wallet: wallet,
-            discovery: { enabled: true, asLocalhost: true }
-        };
-
-        // Connect to gateway using application specified parameters
         console.log('Connect to Fabric gateway.');
-
         await gateway.connect(connectionProfile, connectionOptions);
 
-        // Access EnergyNet network
         console.log('Use network channel: mychannel.');
-
         const network = await gateway.getNetwork('mychannel');
 
-        // Get addressability to solar energy contract
         console.log('Use org.solarnet.solarenergy smart contract.');
-
         const contract = await network.getContract('energycontract');
 
-        // sell solar energy
         console.log('Submit solar energy sell transaction.');
-
-        const organization = connectionProfile.client.organization;
         const sellResponse = await contract.submitTransaction('sell', organization, '00001', '2021-01-25', '2021-02-25', '500kWh');
 
-        // process response
         console.log('Process sell transaction response.' + sellResponse);
-
         let energy = Energy.fromBuffer(sellResponse);
 
         console.log(`${energy.seller} solar energy : ${energy.energyNumber} successfully sell for value ${energy.faceValue}`);
         console.log('Transaction complete.');
 
     } catch (error) {
-
         console.log(`Error processing transaction. ${error}`);
         console.log(error.stack);
 
     } finally {
-
-        // Disconnect from the gateway
         console.log('Disconnect from Fabric gateway.');
         gateway.disconnect();
-
     }
 }
 main().then(() => {
-
     console.log('Issue program complete.');
-
 }).catch((e) => {
-
     console.log('Issue program exception.');
     console.log(e);
     console.log(e.stack);
     process.exit(-1);
-
 });
--- chaincode/application/show_selling.js
+++ chaincode/application/show_selling.js
@@ -7,25 +7,21 @@ const { Wallets, Gateway } = require('fabric-network');
 
 
 async function main() {
-    const userName = process.argv[2];
-
-    if (!userName) {
-        console.log('Please provide an user name.');
-        return;
-    }
+    let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/connection.yaml', 'utf8'));
+    const organization = connectionProfile.client.organization;
+    const userName = 'user' + organization;
 
     const wallet = await Wallets.newFileSystemWallet(`../identity/user/${userName}/wallet`);
     const gateway = new Gateway();
 
+    let connectionOptions = {
+        identity: userName,
+        wallet: wallet,
+        discovery: { enabled: true, asLocalhost: true }
+
+    };
+
     try {
-        let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/connection.yaml', 'utf8'));
-        let connectionOptions = {
-            identity: userName,
-            wallet: wallet,
-            discovery: { enabled: true, asLocalhost: true }
-
-        };
-
         console.log('Connect to Fabric gateway.');
         await gateway.connect(connectionProfile, connectionOptions);
 
