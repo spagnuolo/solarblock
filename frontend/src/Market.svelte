8dae72a058a8ae186d7c2bfa1682fbd4be95c40f
spagnuolo <spagnuol@stud.fra-uas.de>
2021-03-01 17:07:44 +0100

    Ignore fabric-ca aritifacs. Added table for SELLING energy.

--- frontend/src/Market.svelte
+++ frontend/src/Market.svelte
@@ -13,18 +13,42 @@
     }
 </script>
 
-<div class="bg-gray-100 w-full h-full rounded-l-lg shadow-lg text-lg">
+<div class="bg-gray-100 w-full h-full shadow-lg text-lg">
     {#await promise}
         <p>...waiting</p>
     {:then data}
-        {#each data as entry}
-            <p>
-                {entry["Record"].energyNumber}
-                {entry["Record"].seller}
-                {entry["Record"].faceValue} kWh
-            </p>
-        {/each}
+        <table class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th><abbr title="Position">Pos</abbr></th>
+                    <th>ID</th>
+                    <th>Menge</th>
+                    <th>Verkäufer</th>
+                    <th>Datum</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th><abbr title="Position">Pos</abbr></th>
+                    <th>ID</th>
+                    <th>Menge</th>
+                    <th>Verkäufer</th>
+                    <th>Datum</th>
+                </tr>
+            </tfoot>
+            <tbody class="text-center">
+                {#each data as entry, i}
+                    <tr class="hover:bg-gray-600 hover:text-gray-100">
+                        <th>{i + 1}</th>
+                        <td>{entry["Record"].energyNumber}</td>
+                        <td>{entry["Record"].faceValue} kWh</td>
+                        <td>{entry["Record"].seller}</td>
+                        <td>{entry["Record"].sellDateTime}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
     {:catch error}
-        <p style="color: red">{error.message}</p>
+        <p class="bg-red-600 text-gray-100 m-auto">{error.message}</p>
     {/await}
 </div>
