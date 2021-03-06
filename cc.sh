8ed880e5b3c794347bf8ef572cb7b635504f2e88
spagnuolo <spagnuol@stud.fra-uas.de>
2021-03-06 18:27:14 +0100

    old and slow version of cc but maybe stable?

--- cc.sh
+++ cc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CC_VERSION=${1:-"1"}
-
 #Copy the energy contract to every organisations
 \cp -r "chaincode/contract" "organization/haushalt_a/"
 \cp -r "chaincode/contract" "organization/haushalt_b/"
@@ -15,33 +13,63 @@ CC_VERSION=${1:-"1"}
 
 cd organization/haushalt_a
 source iamorghaushalta.sh
-peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label "solar_$CC_VERSION"
+peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label cp_0
 peer lifecycle chaincode install cp.tar.gz
 peer lifecycle chaincode queryinstalled
 
-PACKAGE_ID=$(peer lifecycle chaincode queryinstalled -O json | grep -Po "solar_$CC_VERSION:[[:alnum:]]*")
-peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v $CC_VERSION --package-id $PACKAGE_ID --sequence $CC_VERSION --tls --cafile $ORDERER_CA
+a=$(peer lifecycle chaincode queryinstalled)
+tmp=${a#*ID:}   # remove prefix ending in "ID:"
+b=${tmp%,*}   # remove suffix starting with ","
+echo ${b}
+
+export PACKAGE_ID=${b}
+peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v 0 --package-id $PACKAGE_ID --sequence 1 --tls --cafile $ORDERER_CA
 
 
 cd ../netzbetreiber
 source iamorgnetzbetreiber.sh
-peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label "solar_$CC_VERSION"
+peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label cp_0
 peer lifecycle chaincode install cp.tar.gz
-peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v $CC_VERSION --package-id $PACKAGE_ID --sequence $CC_VERSION --tls --cafile $ORDERER_CA
+peer lifecycle chaincode queryinstalled
+
+a=$(peer lifecycle chaincode queryinstalled)
+tmp=${a#*ID:}   # remove prefix ending in "ID:"
+b=${tmp%,*}   # remove suffix starting with ","
+echo ${b}
+
+export PACKAGE_ID=${b}
+peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v 0 --package-id $PACKAGE_ID --sequence 1 --tls --cafile $ORDERER_CA
 
 
 cd ../haushalt_b
 source iamorghaushaltb.sh
-peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label "solar_$CC_VERSION"
+peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label cp_0
 peer lifecycle chaincode install cp.tar.gz
-peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v $CC_VERSION --package-id $PACKAGE_ID --sequence $CC_VERSION --tls --cafile $ORDERER_CA
+peer lifecycle chaincode queryinstalled
+
+a=$(peer lifecycle chaincode queryinstalled)
+tmp=${a#*ID:}   # remove prefix ending in "ID:"
+b=${tmp%,*}   # remove suffix starting with ","
+echo ${b}
+
+export PACKAGE_ID=${b}
+peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v 0 --package-id $PACKAGE_ID --sequence 1 --tls --cafile $ORDERER_CA
 
 
 cd ../haushalt_c
 source iamorghaushaltc.sh
-peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label "solar_$CC_VERSION"
+peer lifecycle chaincode package cp.tar.gz --lang node --path ./contract --label cp_0
 peer lifecycle chaincode install cp.tar.gz
-peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v $CC_VERSION --package-id $PACKAGE_ID --sequence $CC_VERSION --tls --cafile $ORDERER_CA
+peer lifecycle chaincode queryinstalled
+
+a=$(peer lifecycle chaincode queryinstalled)
+tmp=${a#*ID:}   # remove prefix ending in "ID:"
+b=${tmp%,*}   # remove suffix starting with ","
+echo ${b}
+
+export PACKAGE_ID=${b}
+peer lifecycle chaincode approveformyorg --orderer localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name energycontract -v 0 --package-id $PACKAGE_ID --sequence 1 --tls --cafile $ORDERER_CA
 
 
-peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --peerAddresses localhost:7051 --tlsRootCertFiles ${PEER0_ORGNETZBETREIBER_CA} --peerAddresses localhost:9051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTA_CA} --peerAddresses localhost:18051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTC_CA} --peerAddresses localhost:19051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTB_CA} --channelID mychannel --name energycontract -v $CC_VERSION --sequence $CC_VERSION --tls --cafile $ORDERER_CA --waitForEvent
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --peerAddresses localhost:7051 --tlsRootCertFiles ${PEER0_ORGNETZBETREIBER_CA} --peerAddresses localhost:9051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTA_CA} --peerAddresses localhost:18051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTC_CA} --peerAddresses localhost:19051 --tlsRootCertFiles ${PEER0_ORGHAUSHALTB_CA} --channelID mychannel --name energycontract -v 0 --sequence 1 --tls --cafile $ORDERER_CA --waitForEvent
+docker ps
