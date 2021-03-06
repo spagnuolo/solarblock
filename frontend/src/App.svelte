510a02fc1fe1960767df808b5bb0ab16dd497c43
spagnuolo <spagnuol@stud.fra-uas.de>
2021-03-06 13:38:28 +0100

    Added selection of diffrent panels to frontend.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,23 @@
   import Market from "./Market.svelte";
   import Tailwind from "./Tailwind.svelte";
 
-  const emojis = ["🛒", "💰", "🔍", "🗑️", "⚙️"];
+  const panels = [
+    { name: "Meine Energy", emoji: "⚡", active: true },
+    { name: "Marktplatz", emoji: "🛒", active: false },
+    { name: "", emoji: "🔍", active: false },
+    { name: "", emoji: "🗑️", active: false },
+    { name: "", emoji: "⚙️", active: false },
+  ];
+
+  function handleSelection(index: number) {
+    // Deactivate all.
+    for (let i = 0; i < panels.length; i++) {
+      panels[i].active = false;
+    }
+
+    // Activate selection.
+    panels[index].active = true;
+  }
 </script>
 
 <Tailwind />
@@ -11,15 +27,28 @@
 <main class="w-scree h-screen flex">
   <!-- left sidebar -->
   <div class="bg-gray-100 border-r-2">
-    {#each emojis as emoji}
-      <div class="rounded-full h-16 w-16 m-3 bg-gray-200 hover:bg-gray-300">
+    {#each panels as panel, index}
+      <div
+        class="{panel.active
+          ? 'selected'
+          : ''} rounded-full h-16 w-16 m-3 bg-gray-200 hover:bg-gray-300 cursor-pointer"
+        on:click={() => handleSelection(index)}
+      >
         <div class="flex justify-center items-center w-full h-full text-3xl">
-          {emoji}
+          {panel.emoji}
         </div>
       </div>
     {/each}
   </div>
 
   <!-- main -->
-  <Market />
+  {#if panels[1].active}
+    <Market />
+  {/if}
 </main>
+
+<style>
+  .selected {
+    @apply bg-blue-300;
+  }
+</style>
